4b742fdb6e8fea5c63ae011fbc9826759a2529ab
anika <anikafahmidahossain29@gmail.com>
2022-10-14 17:26:23 +0600

    todolist laravel

---
+++ resources/js/vue/addItemForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="addItem">
+    <input type="text"  v-model="item.name" />
+    <!-- <i class="far fa-plus-square"></i> -->
+    <font-awesome-icon icon="plus-square" 
+    @click="addItem()"
+    :class="[ item.name ? 'active' : 'inactive' , 'plus']" />
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+data: function () {
+  return {
+    item: {
+      name: ""
+    }
+  }
+},
+methods: {
+  addItem() {   
+    console.log("inside add");
+    if (this.item.name == '') {
+    console.log("inside if");
+     return;
+
+      
+    }
+    else {
+      console.log("inside else");
+      axios.post('api/item/store' , {
+        item: this.item
+      })
+      .then( response => {
+        if (response.status == 201) {
+          this.item.name = "";
+          this.$emit('reloadlist');
+        }
+      })
+      .catch ( error => {
+        console.log(error);
+      })
+    }
+   
+  }
+}
+}
+</script>
+
+<style scoped>
+.addItem {
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+input {
+background: #f7f7f7;
+border: 0px;
+outline: none;
+padding: 5px;
+margin-right: 10px;
+width: 100%;
+}
+
+.plus {
+font-size: 20px;
+}
+
+.active {
+color: #00CE25;
+}
+.inactive {
+color: #999999;
+}
+</style>
+
+
+
